88792c1888abad0458189eabf1a04c3c96c0fec4
Dinu Zabrauteanu <dinu.zabrauteanu@gmail.com>
2013-04-20 20:01:18 +0300

    Added website apps authentication

--- startup/getauthenticated.py
+++ startup/getauthenticated.py
@@ -1,20 +1,22 @@
-import urllib2
-import urllib
+import urllib2
+import urllib
+
 
 url='https://smartymirror.com/wp-login.php'
 
-headers = [  ("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.46 Safari/536.5")]
+headers = [ ("User-Agent", "Mozilla/5.0")]
 
-data = [
-    ("log","admin"), 
-    ("pwd","soccer98"), 
-    ("testcookie",1), 
-    ("submit","Log In"), 
-    ("redirect_to","http://smartymirror.com/"), 
-    ("rememberme","forewer")]
+data = [
+	("log","USERNAME"),
+	("pwd","PASSWORD"),
+	("testcookie",1),
+	("submit","Log In"),
+	("redirect_to","http://smartymirror.com/?page_id=30"),
+	("rememberme","forever")]
 
-req = urllib2.Request(url, urllib.urlencode(dict(data)), dict(headers))
-response = urllib2.urlopen(req)
+req = urllib2.Request(url, urllib.urlencode(dict(data)), dict(headers))
+response = urllib2.urlopen(req)
 
 the_page=response.read()
-print the_page
+print the_page
+
